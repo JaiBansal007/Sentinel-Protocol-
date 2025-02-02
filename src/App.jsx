c515cc0ced86e331af9e09f9044015e092f09844
Buggie18 <124082998+Buggie18@users.noreply.github.com>
2025-02-02 11:01:58 +0530

    Roadmap

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,20 @@
-import React from 'react'
-import { Galaxy } from './Components/Galaxy.jsx'
-import './App.css'
-import { ParticleBg } from './Components/ParticleBg.jsx'
-function App() {
+import React from 'react';
+import { Galaxy } from './Components/Galaxy.jsx';
+import { ParticleBg } from './Components/ParticleBg.jsx';
+import Roadmap from './Components/Roadmap';
+import './App.css';
 
+function App() {
   return (
     <>
       <Galaxy />
-      <div className='h-screen w-screen '>
-        
-      <div className='text-white'>asdd</div>
+      <div className="h-screen w-screen">
+        <ParticleBg />
+        <div className="text-white text-center text-2xl mt-10">asdd</div>
+        <Roadmap />
       </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
